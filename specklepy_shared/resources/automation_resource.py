bad0357ab66dde89372e0ee7134bb067e476b6c4
Sebastian Witt <sebastian.witt@rwth-aachen.de>
2025-07-16 12:40:10 +0200

    Added util for easy automation resource access

--- specklepy_shared/resources/automation_resource.py
+++ specklepy_shared/resources/automation_resource.py
@@ -7,7 +7,7 @@ from specklepy.core.api.models.graphql_base_model import GraphQLBaseModel
 from specklepy.core.api.resource import ResourceBase
 from gql import gql
 from specklepy.core.api.responses import DataResponse
-from extended_model_resource import AutomateRunStatus
+from extended_model_resource import AutomateRunStatus, ExtendedModelResource
 
 logger = logging.getLogger("Pipeline")
 NAME = 'automation'
@@ -71,7 +71,7 @@ class AutomationResource(ResourceBase):
             server_version=client.server.server_version,
         )
 
-    def get(self, project_id: str, model_id: str, automation_id: str, model_version_id: str) -> FunctionRunData:
+    def get(self, project_id: str, model_id: str, model_version_id: str, automation_id: Optional[str]) -> FunctionRunData:
         query = gql("""
                         query AutomationData($modelId: String!, $projectId: String!, $versionId: String!) {
                           project(id: $projectId) {
@@ -100,13 +100,46 @@ class AutomationResource(ResourceBase):
             AutomationData, query, variables
         )
         automation = next(a for a in data.project.model.version.automationsStatus.automationRuns
-                          if a.automationId == automation_id)
+                          if automation_id is None or a.automationId == automation_id)
         return next(run for run in automation.functionRuns if run.status == AutomateRunStatus.Succeeded)
 
-    def try_get(self, project_id: str, model_id: str, automation_id: str, model_version_id: str) -> Optional[FunctionRunData]:
+    def try_get(self, project_id: str, model_id: str, model_version_id: str, automation_id: Optional[str]) -> Optional[FunctionRunData]:
         try:
-            return self.get(project_id, model_id, automation_id, model_version_id)
+            return self.get(project_id, model_id, model_version_id, automation_id)
         except Exception as e:
             logger.error(e)
             return None
 
+    @staticmethod
+    def get_last_successful_automation(speckle_client: SpeckleClient,
+                                       model_id: str,
+                                       project_id: str,
+                                       automation_id: Optional[str]) -> Optional[AutomationRunData]:
+        model = ExtendedModelResource(speckle_client)
+        model_data = model.get_with_versions(model_id,
+                                             project_id, versions_limit=50)
+
+        logger.info("; ".join([str(version) + " " +
+                               str(version.automations_status and
+                                   version.automations_status.status == AutomateRunStatus.Succeeded)
+                               for version in model_data.versions.items]))
+
+        success_version_id = None
+        try:
+            success_version_id = next(iter(version for version in sorted(model_data.versions.items,
+                                                                         key=lambda x: x.created_at, reverse=True)
+                                           if version.automations_status and
+                                           version.automations_status.status == AutomateRunStatus.Succeeded)).id
+            logger.info(f"Success version: {success_version_id}")
+        except StopIteration:
+            if model_data.versions.total_count > 50:
+                logger.warning(f"Couldn't find a successful automation in the last 50 versions.")
+
+        data = None
+        if success_version_id is not None:
+            automation = AutomationResource(speckle_client)
+            data = automation.try_get(project_id,
+                                      model_id,
+                                      success_version_id,
+                                      automation_id)
+        return data
